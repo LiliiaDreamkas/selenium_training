5ee0c3255c3bdde193b719e23f5e499dd508833d
LiliiaMikhareva <liliya.mikhareva@yandex.ru>
2018-11-03 14:07:21 +0300

    Use other browsers

--- src/test/java/ru/stqa/training/selenium/TestBase.java
+++ src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -4,7 +4,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 public class TestBase {
 
@@ -14,6 +22,11 @@ public class TestBase {
     @Before
     public void start() {
         driver = new ChromeDriver();
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability("firefox_binary", "C:\\Program Files\\Firefox Nightly\\firefox.exe");
+//        driver = new FirefoxDriver(caps);
+//        driver = new InternetExplorerDriver();
+//        driver = new EdgeDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
