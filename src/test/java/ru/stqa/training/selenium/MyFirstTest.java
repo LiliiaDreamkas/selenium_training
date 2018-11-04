e6ed5fef0463883b8102d979bb61e3e4fe6bb095
LiliiaMikhareva <liliya.mikhareva@yandex.ru>
2018-11-04 15:54:02 +0300

    Parallel start

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,12 +1,16 @@
 package ru.stqa.training.selenium;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
-public class MyFirstTest extends TestBase{
+public class MyFirstTest extends TestBase {
 
     @Test
-    public void myFirstTest() {
+    public void myFirstTest() throws InterruptedException {
         driver.get("http://www.google.com/");
+        Thread.sleep(30000);
+        driver.findElement(By.name("q")).sendKeys("selenium");
+        driver.findElement(By.name("btnK")).click();
     }
 
 }
--- src/test/java/ru/stqa/training/selenium/TestBase.java
+++ src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -16,23 +16,32 @@ import java.io.File;
 
 public class TestBase {
 
-    WebDriver driver;
-    WebDriverWait wait;
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    public WebDriver driver;
+    public WebDriverWait wait;
 
     @Before
     public void start() {
+        if (tlDriver.get() != null) {
+            driver = tlDriver.get();
+            wait = new WebDriverWait(driver, 10);
+            return;
+        }
         driver = new ChromeDriver();
 //        DesiredCapabilities caps = new DesiredCapabilities();
 //        caps.setCapability("firefox_binary", "C:\\Program Files\\Firefox Nightly\\firefox.exe");
 //        driver = new FirefoxDriver(caps);
 //        driver = new InternetExplorerDriver();
 //        driver = new EdgeDriver();
+        tlDriver.set(driver);
         wait = new WebDriverWait(driver, 10);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> { driver.quit(); driver = null; }));
     }
 
     @After
     public void stop() {
-        driver.quit();
-        driver = null;
+//        driver.quit();
+//        driver = null;
     }
 }
