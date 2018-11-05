30a6967726b2dcd831a00ebcfa12a2bb8fc9184c
LiliiaMikhareva <liliya.mikhareva@yandex.ru>
2018-11-05 18:13:58 +0300

    Check stickers

---
+++ src/test/java/ru/stqa/training/selenium/CheckStickers.java
@@ -0,0 +1,22 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class CheckStickers extends TestBase {
+
+    @Test
+    public void checkStickers() {
+        driver.get("http://localhost/litecart/");
+        List<WebElement> products = driver.findElements(By.cssSelector("div.image-wrapper"));
+        for (int i = 0; i < products.size(); i++) {
+            WebElement currentProduct = products.get(i);
+            List<WebElement> stickers = currentProduct.findElements(By.cssSelector("div [class *= sticker]"));
+            Assert.assertTrue("Current element has "  + stickers.size() + " stickers", stickers.size() == 1);
+        }
+    }
+}
