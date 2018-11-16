171fd1e604a844751f16e81e20272f1f022c55f1
LiliiaDreamkas <liliya.mikhareva@yandex.ru>
2018-11-16 12:57:07 +0300

    Check product price

--- src/test/java/ru/stqa/training/selenium/CheckProductPageTest.java
+++ src/test/java/ru/stqa/training/selenium/CheckProductPageTest.java
@@ -19,4 +19,23 @@ public class CheckProductPageTest extends TestBase{
                 currentProductName, currentProductNameOnProductPage),
                 currentProductName.equals(currentProductNameOnProductPage));
     }
+
+    @Test
+    public void checkPrice() {
+        driver.get("http://localhost/litecart/");
+        WebElement currentProduct = driver.findElement(By.cssSelector("div#box-campaigns li[class *= product]"));
+        String regularPrice = currentProduct.findElement(By.cssSelector("s.regular-price")).getText();
+        String campaignPrice = currentProduct.findElement(By.cssSelector("strong.campaign-price")).getText();
+        currentProduct.click();
+        String regularPriceOnProductPage = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getText();
+        String campaignPriceOnProductPage = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getText();
+
+        Assert.assertTrue(String.format("Regular price on general page (\"%s\") and home page (\"%s\") don't match",
+                regularPrice, regularPriceOnProductPage),
+                regularPrice.equals(regularPriceOnProductPage));
+
+        Assert.assertTrue(String.format("Campaign price on general page (\"%s\") and home page (\"%s\") don't match",
+                campaignPrice, campaignPriceOnProductPage),
+                campaignPrice.equals(campaignPriceOnProductPage));
+    }
 }
