edc42ffbe988d1c2db7cb46a1fd8220844df711b
LiliiaDreamkas <liliya.mikhareva@yandex.ru>
2018-11-16 17:29:02 +0300

    Check campaign price

--- src/test/java/ru/stqa/training/selenium/CheckProductPageTest.java
+++ src/test/java/ru/stqa/training/selenium/CheckProductPageTest.java
@@ -1,11 +1,10 @@
 package ru.stqa.training.selenium;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
+import static org.junit.Assert.*;
 
 public class CheckProductPageTest extends TestBase{
 
@@ -18,7 +17,7 @@ public class CheckProductPageTest extends TestBase{
         currentProduct.click();
         String currentProductNameOnProductPage = driver
                 .findElement(By.cssSelector("div#box-product h1[itemprop = name]")).getText();
-        Assert.assertTrue(String.format("Name on general page (\"%s\") and home page (\"%s\") don't match",
+        assertTrue(String.format("Name on general page (\"%s\") and home page (\"%s\") don't match",
                 currentProductName, currentProductNameOnProductPage),
                 currentProductName.equals(currentProductNameOnProductPage));
     }
@@ -34,11 +33,11 @@ public class CheckProductPageTest extends TestBase{
         String regularPriceOnProductPage = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getText();
         String campaignPriceOnProductPage = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getText();
 
-        Assert.assertTrue(String.format("Regular price on general page (\"%s\") and home page (\"%s\") don't match",
+        assertTrue(String.format("Regular price on general page (\"%s\") and home page (\"%s\") don't match",
                 regularPrice, regularPriceOnProductPage),
                 regularPrice.equals(regularPriceOnProductPage));
 
-        Assert.assertTrue(String.format("Campaign price on general page (\"%s\") and home page (\"%s\") don't match",
+        assertTrue(String.format("Campaign price on general page (\"%s\") and home page (\"%s\") don't match",
                 campaignPrice, campaignPriceOnProductPage),
                 campaignPrice.equals(campaignPriceOnProductPage));
     }
@@ -48,26 +47,61 @@ public class CheckProductPageTest extends TestBase{
     public void checkRegularPriceParameters() {
         driver.get("http://localhost/litecart/");
         WebElement currentProduct = driver.findElement(By.cssSelector("div#box-campaigns li[class *= product]"));
-        String linePresence = currentProduct.findElement(By.cssSelector("s.regular-price")).getCssValue("text-decoration");
-        Assert.assertTrue("Regular price on general page is not crossed", linePresence.contains("line-through"));
+        String priceTag = currentProduct.findElement(By.cssSelector("s.regular-price")).getTagName();
+        checkThatElementHaveLine(priceTag);
         String regularPriceColor = currentProduct.findElement(By.cssSelector("s.regular-price")).getCssValue("color");
-        checkColorValue(regularPriceColor);
+        checkColorValue(regularPriceColor, "gray");
+
         currentProduct.click();
+
         WebElement regPrice = driver.findElement(By.cssSelector("div#box-product s.regular-price"));
-        linePresence = regPrice.getCssValue("text-decoration");
-        Assert.assertTrue("Regular price on product page is not crossed", linePresence.contains("line-through"));
+        priceTag = regPrice.getTagName();
+        checkThatElementHaveLine(priceTag);
         regularPriceColor = regPrice.getCssValue("color");
-        checkColorValue(regularPriceColor);
+        checkColorValue(regularPriceColor, "gray");
     }
 
-    private void checkColorValue(String regularPriceColor) {
+    //г
+    @Test
+    public void checkCampaignPriceParameters() {
+        driver.get("http://localhost/litecart/");
+        WebElement currentProduct = driver.findElement(By.cssSelector("div#box-campaigns li[class *= product]"));
+        String priceTag = currentProduct.findElement(By.cssSelector("strong.campaign-price")).getTagName();
+        checkThatElementIsBold(priceTag);
+        String regularPriceColor = currentProduct.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
+        checkColorValue(regularPriceColor, "red");
+
+        currentProduct.click();
+
+        WebElement campaignPrice = driver.findElement(By.cssSelector("div#box-product strong.campaign-price"));
+        priceTag = campaignPrice.getTagName();
+        checkThatElementIsBold(priceTag);
+        regularPriceColor = campaignPrice.getCssValue("color");
+        checkColorValue(regularPriceColor, "red");
+    }
+
+    private void checkThatElementHaveLine(String elementTag) {
+        assertTrue("Element is not crossed", (elementTag.equals("s") || elementTag.equals("strike") || elementTag.equals("strike")));
+    }
+
+    private void checkThatElementIsBold(String elementTag) {
+        assertTrue("Element is not bold", (elementTag.equals("strong") || elementTag.equals("b")));
+    }
+
+    private void checkColorValue(String regularPriceColor, String color) {
         regularPriceColor = regularPriceColor.replace("rgb", "").replace("a", "").replace("(", "")
                 .replace(")", "").replace(" ", "");
         String[] colorParameters = regularPriceColor.split(",");
         String r = colorParameters[0];
         String g = colorParameters[1];
         String b = colorParameters[2];
-        Assert.assertTrue("Regular price is not gray",
-                (r.equals(g) && g.equals(b)));
+        if (color.equals("gray")) {
+            assertTrue("Regular price is not gray",
+                    (r.equals(g) && g.equals(b)));
+        } else if (color.equals("red")) {
+            assertTrue("Campaign price is not red",
+                    (g.equals("0") && b.equals("0")));
+        }
+
     }
 }
